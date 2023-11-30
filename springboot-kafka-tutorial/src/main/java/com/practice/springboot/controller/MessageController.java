4d6eecf1527dba641afd9dea4c3d277ce6ba9228
sayal-aryal <sayalaryal2@gmail.com>
2023-11-29 21:25:52 -0600

    Adding Kafka consumer and producer to send the message to the topic and consume message from topic

---
+++ springboot-kafka-tutorial/src/main/java/com/practice/springboot/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.practice.springboot.controller;
+
+import com.practice.springboot.kafka.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    private KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    //https:localhost:8080/api/v1/kafka/publish?message=hello world
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message) {
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message Sent to the topic");
+    }
+}
