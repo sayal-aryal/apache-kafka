ed386714920d41b7b84729d06d51e477eeb48fe8
sayal-aryal <sayalaryal2@gmail.com>
2023-11-30 19:59:53 -0600

    Created kafka consumer and producer and rest api to send JSON Object.

--- springboot-kafka-tutorial/src/main/java/com/practice/springboot/kafka/JsonKafkaProducer.java
+++ springboot-kafka-tutorial/src/main/java/com/practice/springboot/kafka/JsonKafkaProducer.java
@@ -26,7 +26,7 @@ public class JsonKafkaProducer {
         LOGGER.info(String.format("Message sent -> %s", data.toString()));
         Message<User> message = MessageBuilder
                 .withPayload(data)
-                .setHeader(KafkaHeaders.TOPIC,"javaguides")
+                .setHeader(KafkaHeaders.TOPIC,"javaguides_json")
                 .build();
 
         kafkaTemplate.send(message);
